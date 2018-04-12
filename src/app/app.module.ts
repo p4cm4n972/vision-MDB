9976d4eb84854ba95cec44d5b93745827b984248
p4cm4n972 <manuel.adele@gmail.com>
2018-04-12 15:24:36 +0200

    update READ ME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+// MDBootstrap
+import { MDBBootstrapModules } from 'ng-mdb-pro';
+import { MDBSpinningPreloader } from 'ng-mdb-pro';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+// MAPS
+import { AgmCoreModule } from '@agm/core';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MDBBootstrapModules.forRoot(),
+    AppRoutingModule,
+    // AgmCoreModule.forRoot({ apiKey: 'Your_api_key' })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MDBSpinningPreloader,
+  ],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
